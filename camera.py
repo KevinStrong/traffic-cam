b72556470dbc3bd1f06d97eee382cf8074c908c8
Kevin C Strong <kevin.colby.strong@gmail.com>
2019-04-09 18:02:34 -0500

    add timestamp to camera.py with native pi commands

--- camera.py
+++ camera.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 import sys
-import picamera 
+import picamera
+from picamera import Color
 from time import sleep
 from subprocess import call
 from datetime import datetime
 
+
 file_name = sys.argv[1]
 #fileName ="/home/pi/cam/picture.jpg"
 
+currentTime = datetime.now()
+timestampMessage = currentTime.strftime("%Y.%m.%d-%H:%M:%S") 
 
 with picamera.PiCamera() as camera:
     sleep(1)
     camera.resolution = (1280, 720)
+    camera.annotate_foreground = Color('red')
+    camera.annotate_text = timestampMessage 
     camera.capture(file_name)
