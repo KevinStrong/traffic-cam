55d692871dad4669e9702165fce37f85cbe0211d
Kevin C Strong <kevin.colby.strong@gmail.com>
2019-04-09 18:20:12 -0500

    add native timestamp to camera_timestamp.py and actually generate an image

--- camera_timestamp.py
+++ camera_timestamp.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
+import sys
 import picamera
 import picamera.array
 import numpy as np
 from time import sleep
-#from subprocess import call
-#from datetime import datetime
+from datetime import datetime
+from picamera import Color
 
-fileName ="/home/pi/cam/houston_traffic.jpg"
+file_name = sys.argv[1]
 THRESHOLD = 60
 
 with picamera.PiCamera() as camera:
-    sleep(0.1) #let camera start up
     camera.resolution = (1280, 720)
     camera.framerate = 24
     with picamera.array.PiYUVArray(camera) as stream:
+        sleep(1)
         camera.capture(stream, format='yuv', use_video_port=True)
         average = np.average(stream.array[..., 0])
         if average > THRESHOLD:
             camera.exposure_compensation -= int((average - THRESHOLD) /10)
+        currentTime = datetime.now()
+        timestampMessage = currentTime.strftime("%Y.%m.%d-%H:%M:%S") 
+        camera.annotate_foreground = Color('red')
+        camera.annotate_text = timestampMessage 
+        sleep(1)
+        camera.capture(file_name)
 
-#currentTime = datetime.now()
-#timestampMessage = currentTime.strftime("%Y.%m.%d-%H:%M:%S") 
-#timestampCommand = "/usr/bin/convert " + fileName + " -pointsize 32 \
- #       -fill red -annotate +975+700 '" + timestampMessage + "' " + fileName
-#call([timestampCommand], shell=True)
