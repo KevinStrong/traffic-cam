4a4ff21afe3185c3b4bdc2400bf0ca4237d7fdd4
Kevin C Strong <kevin.colby.strong@gmail.com>
2019-04-09 19:55:24 -0500

    add ability to upload content to a aws subdirectory in s3

--- aws_upload.py
+++ aws_upload.py
@@ -2,15 +2,19 @@
 import boto3
 import sys
 
-file_name = sys.argv[1]
 bucket = 'narcindin.com'
+local_file_name = sys.argv[1]
+
+if len(sys.argv) > 2:
+    aws_file_name = sys.argv[2] + "/" + local_file_name
+else:
+    aws_file_name = local_file_name
 
 metadata = {"Content-Type" : "image/jpeg"}
 
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(bucket)
-data = open(file_name, 'rb');
-bucket.put_object(Key=file_name, Body=data, ContentType='image/jpeg')
-#s3.Object(bucket, file_name).put(Key=file_name, Body=file_name, Metadata=metadata)
+data = open(local_file_name, 'rb');
+bucket.put_object(Key=aws_file_name, Body=data, ContentType='image/jpeg')
 
