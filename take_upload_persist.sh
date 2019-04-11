deddec2d3a0e26d19e7490a2d0b825dc0b6fa59a
Kevin C Strong <kevin.colby.strong@gmail.com>
2019-04-10 22:26:30 -0500

    Fix logic to upload timestamped image to 'daily' folder

--- take_upload_persist.sh
+++ take_upload_persist.sh
@@ -3,11 +3,17 @@
 script_folder='/home/pi/cam'
 cd $script_folder
 
+folder="daily"
+
 image="houston_traffic.jpg"
 timestamp=$(date +%H:%M)
 timestamp_image="houston_traffic-$timestamp.jpg"
 
+#Upload as most recent image
 python3 $script_folder/camera_adjusted.py $image
 python3 $script_folder/aws_upload.py $image
+
+#Upload as timestamped image
 mv $image $timestamp_image
 python3 $script_folder/aws_upload.py $timestamp_image $folder
+mv $timestamp_image $folder
